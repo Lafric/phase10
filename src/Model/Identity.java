a3347ce0f9326cb5507b4d5f82de74077e44edd3
Schnilz <nils.l1701@gmail.com>
2023-07-05 16:30:25 +0200

    added currentlobby

--- src/Model/Identity.java
+++ src/Model/Identity.java
@@ -4,10 +4,19 @@ import java.io.Serializable;
 public class Identity implements Serializable {
     private String username;
 
+    private String currentLobby; 
+
+
     public Identity(String name) {
         this.username = name;
     }
 
+    public void setCurrentLobby(String lobbyName){
+        this.currentLobby = lobbyName; 
+    }
+    public String getCurrentLobby(){
+        return this.currentLobby; 
+    }   
 
     public String getUsername() {
         return this.username;

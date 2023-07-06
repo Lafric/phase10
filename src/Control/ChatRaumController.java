5136e7c8c25961993992d6aed911ae40f7475f58
Lafric <billydongmo07@gmail.com>
2023-07-06 17:29:20 +0200

    chat raum

---
+++ src/Control/ChatRaumController.java
@@ -0,0 +1,13 @@
+package Control;
+
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
+public class ChatRaumController {
+    public TextArea raumChat_ausgabe;
+    public TextField raumChat_eingabe;
+
+    public void sendeNachricht_raumChat(KeyEvent keyEvent) {
+    }
+}

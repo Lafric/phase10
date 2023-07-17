e51d718d6a78735346435b2fb55bf70c13eb27be
buhlien <julien.jegorovs@gmail.com>
2023-07-17 19:27:42 +0200

    ich hasse dich Nils

--- src/Communication/RMIServer.java
+++ src/Communication/RMIServer.java
@@ -1,26 +1,23 @@
 package Communication;
 
+import Communication.ServerFuncsImpl;
+import java.rmi.Remote;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
-import java.rmi.server.UnicastRemoteObject;
 
-
-
-
-public class RMIServer implements Runnable {
-//Server class to run on the server side 
-//Bind serverfunctions to rmiregistry 
-    @Override
-    public void run() {
+public class RMIServer {
+    public static void main(String[] arrstring) {
+        System.setProperty("java.rmi.server.hostname", "185.162.248.237");
         try {
-            Registry registry = LocateRegistry.createRegistry(1099);
-            ServerFuncsImpl serverfuncs = new ServerFuncsImpl();
-            ServerFuncs ServerfuncsObj = (ServerFuncs) UnicastRemoteObject.exportObject(serverfuncs, 0);
-            registry.bind("serverfunc", ServerfuncsObj);
-            System.out.println("Server is ready.");
-        } catch (Exception e) {
-            System.err.println("Server exception: " + e.toString());
-            e.printStackTrace();
+            ServerFuncsImpl serverFuncsImpl = new ServerFuncsImpl();
+            System.out.println("Created Remote Object");
+            Registry registry = LocateRegistry.getRegistry("185.162.248.237", 1099);
+            System.out.println("Found registry");
+            registry.bind("serverfunc", (Remote)serverFuncsImpl);
+            System.out.println("Server ready");
+        }
+        catch (Exception exception) {
+            System.out.println("Some error..." + exception);
         }
     }
 }

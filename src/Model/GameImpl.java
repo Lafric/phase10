1bca41939adbfd29ff1fdc3cf1e1ad042f80e6c2
Lafric <billydongmo07@gmail.com>
2023-07-17 20:21:59 +0200

    "laycard "

--- src/Model/GameImpl.java
+++ src/Model/GameImpl.java
@@ -427,9 +427,11 @@ public class GameImpl extends UnicastRemoteObject implements Game {
                     for (int j = 0; j < cards.length; j++) {
                         System.out.println("REMOVING CARD " + cards[j].getId());
                         System.out.println("length CARD before" + player.getHandCards().size());
+                        System.out.println("current player before" +  player.getName() + " " + player.getId());
                         player.removeCard(cards[j]);
 
                         System.out.println("length CARD after" + player.getHandCards().size());
+                        System.out.println("current player after" +  player.getName() + " " + player.getId());
                         for(Card card : player.getHandCards()){
                             System.out.println( "card after" + card);
                         }
